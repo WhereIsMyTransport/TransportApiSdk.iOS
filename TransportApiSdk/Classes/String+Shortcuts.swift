b008426849d8f29e603647924dfd40ae2da5fdb7
Bilo <bilo@wimt.co.za>
2017-01-28 18:14:41 +0200

    More cleanup of agencies and adding some error checking

---
+++ TransportApiSdk/Classes/String+Shortcuts.swift
@@ -0,0 +1,9 @@
+//
+//  QueryString+Shortcuts.swift
+//  Pods
+//
+//  Created by Bilo on 1/28/17.
+//
+//
+
+import Foundation
---
+++ TransportApiSdk/Classes/TransportApiResult.swift
@@ -0,0 +1,9 @@
+//
+//  TransportApiResult.swift
+//  Pods
+//
+//  Created by Bilo on 1/28/17.
+//
+//
+
+import Foundation
